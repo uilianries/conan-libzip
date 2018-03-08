85e0f831b8b32a35b8386f23c455027f11e82a41
Jerry W <jerrywiltse@gmail.com>
2018-03-07 19:32:14 -0500

    refresh all

--- conanfile.py
+++ conanfile.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-# conanfile for build libzip (chosen because it only depends on zlib)
 
 from conans import ConanFile, CMake, tools
 
@@ -8,65 +7,44 @@ import os
 
 
 class LibZipConan(ConanFile):
-  name = 'libzip'
-  version = '1.2.0'
-  license = 'bsd'
-  url = 'https://github.com/k0ekk0ek/conan-libzip'
-  generators = 'cmake', 'txt'
-  settings = 'os', 'compiler', 'build_type', 'arch'
-  options = {'shared': [True, False]}
-  default_options = 'shared=True'
+    name = "libzip"
+    description = "A C library for reading, creating, and modifying zip archives"
+    version = "1.2.0"
+    url = "https://github.com/bincrafters/conan-libzip"
+    homepage = "https://github.com/nih-at/libzip"
+    license = "BSD"
+    exports = ["LICENSE.md"]
+    exports_sources = ["CMakeLists.txt"]
+    generators = "cmake"
+    source_subfolder = "source_subfolder"
+    build_subfolder = "build_subfolder"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False]}
+    default_options = "shared=True"
+    requires = "zlib/1.2.11@conan/stable"
 
-  # Convenience properties not required by Conan
-  directory = '{}-{}'.format(name, version)
-  filename = '{}.tar.gz'.format(directory)
-  checksum = '6cf9840e427db96ebf3936665430bab204c9ebbd0120c326459077ed9c907d9f'
+    def source(self):
+        source_url = "https://libzip.org/download"
+        tools.get("{0}/{1}-{2}.tar.gz".format(source_url, self.name, self.version))
+        extracted_dir = self.name + "-" + self.version
+        os.rename(extracted_dir, self.source_subfolder)
 
-  def source(self):
-    tools.download(
-      'https://nih.at/libzip/{}'.format(self.filename), self.filename)
-    tools.check_sha256(self.filename, self.checksum)
-    tools.unzip(self.filename, '.')
-    os.unlink(self.filename)
-  # source
+    def configure_cmake(self):
+        cmake = CMake(self)
+        cmake.definitions["BUILD_SHARED_LIBS"] = self.options.shared
+        cmake.configure()
+        return cmake
+        
+    def build(self):
+        cmake = self.configure_cmake()
+        cmake.build()
 
-  def configure(self):
-    self.requires.add("zlib/1.2.11@conan/stable", private=False)
-    self.options['zlib'].shared = self.options.shared
-  # configure
+    def package(self):
+        cmake = self.configure_cmake()
+        cmake.install()
+        self.copy(pattern="LICENSE", dst="licenses", src=self.source_subfolder)
 
-  def build(self):
-    # Add inclusion of conanbuildinfo.cmake otherwise libzip will use the
-    # version of zlib shipped with the platform
-    cmakelists = '{}/CMakeLists.txt'.format(self.directory)
-    search = 'CMAKE_MINIMUM_REQUIRED(VERSION 2.6)'
-    replace = '''PROJECT(libzip C)
-CMAKE_MINIMUM_REQUIRED(VERSION 3.0)
-include(../conanbuildinfo.cmake)
-CONAN_BASIC_SETUP()
-'''
-
-    tools.replace_in_file(cmakelists, search, replace)
-
-    # Update libzip-x.x/lib/CMakeLists.txt to enable creating a static library
-    # FIXME: implement
-
-    # Build the sources using CMake
-    cmake = CMake(self)
-    self.run('cmake {} {}'.format(self.directory, cmake.command_line))
-    self.run('cmake --build . {}'.format(cmake.build_config))
-  # build
-
-  def package(self):
-    # Header files
-    self.copy('zip.h', dst='include',  src=self.directory, keep_path=False)
-    self.copy('zipconf.h', dst='include', keep_path=False)
-    self.copy('config.h', dst='include/libzip', keep_path=False)
-    # Libraries
-    self.copy('*.lib', dst='lib', keep_path=False)
-    self.copy('*.dll', dst='bin', keep_path=False)
-    if self.options.shared:
-      self.copy('*.so', dst='lib', keep_path=False)
-    self.copy('*.a', dst='lib', keep_path=False)
-  # package
-
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.includedirs.append(os.path.join("lib", "libzip", "include"))
+   
---
+++ test_package/test_package.c
@@ -0,0 +1,8 @@
+#include <zip.h>
+
+int main()
+{
+	zip_source_t *src;
+	return 0;
+}
+
